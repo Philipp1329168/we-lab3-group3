d5fcf88e93d756206fc0beca5fd7b60c585524a0
patrick <patrick.weiszkirchner@gmail.com>
2015-05-09 00:21:20 +0200

    Commit languages

--- app/controllers/JeopardyController.java
+++ app/controllers/JeopardyController.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.big.we15.lab2.api.*;
 import at.ac.tuwien.big.we15.lab2.api.impl.PlayJeopardyFactory;
 
 import at.ac.tuwien.big.we15.lab2.api.impl.SimpleJeopardyGame;
+import play.cache.Cache;
 import play.i18n.Messages;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -21,6 +22,7 @@ public class JeopardyController extends Controller {
     public static Result showJeopardyPage() {
         JeopardyFactory factory = new PlayJeopardyFactory(Messages.get("json.file"));
         JeopardyGame game = new SimpleJeopardyGame(factory);
+        Cache.set(session("username"), game);
         return ok(jeopardy.render(game));
     }
 }
--- app/controllers/QuestionController.java
+++ app/controllers/QuestionController.java
@@ -4,7 +4,7 @@ import at.ac.tuwien.big.we15.lab2.api.*;
 import at.ac.tuwien.big.we15.lab2.api.impl.PlayJeopardyFactory;
 
 import at.ac.tuwien.big.we15.lab2.api.impl.SimpleJeopardyGame;
-import play.i18n.Messages;
+import play.cache.Cache;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
@@ -16,6 +16,15 @@ import views.html.question;
 
 public class QuestionController extends Controller {
     public static Result showQuestionPage(){
-        return ok(question.render(game));
+        SimpleJeopardyGame game = (SimpleJeopardyGame)Cache.get(session("username"));
+        return ok(question.render(game, checkSession()));
+    }
+
+    private static boolean checkSession() {     // nur für temporäre Zwecke
+        if(session("username")==null) {         // Lösnug vllt über Überprüfung des game-Objekts?
+            return false;
+        } else {
+            return true;
+        }
     }
 }
